d78f916134ef4df4c78ee11833dd54eb839ed16b
jsersan <jsersan@gmail.com>
2022-03-29 18:26:02 +0200

    appReact Carrito compra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import Producto from './components/Producto'
+import Carrito from './components/Carrito'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ // Crear listado de productos
+
+ // Los states no se pueden modificar directamente. 
+ // Debe hacerse a través de useState o anteriormente setState
+
+const [productos, guardarProductos ]= useState([
+  {id:1, nombre: 'Camisa React JS',precio: 50},
+  {id:2, nombre: 'Pantalón React JS',precio: 150},
+  {id:3, nombre: 'Blazer React JS',precio: 120},
+  {id:4, nombre: 'Chaqueta React JS',precio: 250}
+]);
+
+// State para el carrito de compras
+
+const [carrito, agregarProducto] = useState ([]);
+
+
+// Obtener la fecha
+
+const fecha = new Date().getFullYear();
+
+ return (
+    // eslint-disable-next-line react/jsx-no-undef
+    <div>
+
+      <Header
+          titulo = 'Tienda Virtual'
+          numero = "20"
+      />
+    
+
+    <h1>Lista de Productos</h1>
+    { productos.map( producto => (
+      <Producto 
+          key = {producto.id}
+          producto =  { producto }
+          carrito = {carrito}
+          productos = {productos}
+          agregarProducto = {agregarProducto}  />
+    ))}
+
+    <Carrito 
+        carrito = {carrito}
+        agregarProducto = {agregarProducto}
+    />
+
+    <Footer 
+    
+      fecha = {fecha}
+    
+    />
     </div>
-  );
+
+);
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// imr
+
+import React from 'react';
+
+// sfc
+const Footer = ({fecha}) => (
+        <footer>
+            <p>Todos los derechos reservados &copy; {fecha }</p>
+        </footer>
+);
+ 
+export default Footer;
